1abd4c9bb9f46d52ee0b1d72512fff8486929c8a
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-11-30 19:37:12 -0700

    chore: Add TurretAttack.

---
+++ Tower Defense Course/Assets/Scripts/TurretAttack.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class TurretAttack : MonoBehaviour
+{
+    [field: SerializeField]
+    public AreaOfEngagement AoE { get; private set;}
+    [field: SerializeField]
+    public Projectile ProjectilePrefab { get; private set;}
+    [field: SerializeField]
+    public float CooldownTime { get; private set; } = 3;
+    [field: SerializeField]
+    public bool IsOnCooldown { get; private set; } = false;
+
+    void Update()
+    {
+        if (IsOnCooldown) { return; }
+        if (AoE.Targets.Count == 0) { return; }
+        Fire();
+        IsOnCooldown = true;
+        Invoke(nameof(ResetCooldown), CooldownTime);
+    }
+
+    private void ResetCooldown() => IsOnCooldown = false;
+
+    private void Fire()
+    {
+        Projectile newProjectile = Instantiate(ProjectilePrefab);
+        // Sets the projectile's position to match the turret's position
+        newProjectile.transform.position = transform.position;
+        // Sets the projectile's target to the first target in the AoE
+        newProjectile.Target = AoE.Targets[0];
+    }
+}
+
