34d4dc6dbe8f23bf3b5475a093ed60a5b9cb7f4c
Captain Coder <captaincodersacademy@gmail.com>
2025-05-09 06:16:44 -0700

    chore: First draft of part 24

--- Tower Defense Course/Assets/Scripts/Clickable.cs
+++ Tower Defense Course/Assets/Scripts/Clickable.cs
@@ -2,9 +2,13 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
-public class Clickable : MonoBehaviour, IPointerClickHandler
+public class Clickable : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
     [field: SerializeField] public UnityEvent OnClick { get; private set; }
+    [field: SerializeField] public UnityEvent OnEnter { get; private set; }
+    [field: SerializeField] public UnityEvent OnExit { get; private set; }
     public void OnPointerClick(PointerEventData eventData) => OnClick.Invoke();
+    public void OnPointerEnter(PointerEventData eventData) => OnEnter.Invoke();
+    public void OnPointerExit(PointerEventData eventData) => OnExit.Invoke();
 }
 
